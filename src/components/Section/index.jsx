676efe591a40f0f6fef94b4fc9fafc4dbf30cfb7
Isaac Ramos <isaacdeferro@gmail.com>
2024-05-20 08:58:08 -0300

    feat: adicionar section

---
+++ src/components/Section/index.jsx
@@ -0,0 +1,35 @@
+import './index.css'
+
+export default function Section1() {
+    return(
+        <div id="site">
+            <div className="InfoSite">
+                <h1 className="TituloSite">Título do Site</h1>
+                <p className="TextoSite">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae praesentium impedit
+                    perspiciatis, maiores ut
+                    ab est repudiandae quia natus, consequuntur id voluptatum! Accusamus numquam et odit hic. Consequatur,
+                    numquam distinctio! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae impedit minima
+                    consequatur maiores, velit eum architecto accusamus laborum placeat nemo corrupti a, nisi reprehenderit
+                    suscipit nostrum perferendis pariatur quibusdam quasi. Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit.
+                    <br/>
+                    <br/>
+                    Minus eos repellat quidem earum veritatis? Facilis at nulla ut quasi voluptates animi iusto autem, tempore
+                    perferendis eos saepe velit, quaerat quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+                    blanditiis unde iure velit sit accusantium earum provident minima, hic, fugiat consectetur est alias quam
+                    harum! Ducimus dicta sint quis? Expedita. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero,
+                    eius odit quam laboriosam alias at quidem, corrupti repellendus aspernatur sed commodi facilis et accusamus
+                    vel placeat voluptatem libero perspiciatis modi!
+                </p>
+                <button id="BotaoSite">
+                    Acesse o Site
+                </button>
+            </div>
+            <div className="ImagemSite">
+                <img src="https://i.pinimg.com/originals/39/ad/f6/39adf65a1488e578c6ba8fef78cae9a1.jpg"
+                    alt=""/>
+            </div>
+
+        </div>
+    )
+}
--- src/index/index.jsx
+++ src/index/index.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import './index.css'
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal'
 import 'reactjs-popup/dist/index.css';
+import Section1 from '../components/Section';
 import { useEffect } from 'react';
 
 
@@ -41,7 +41,7 @@ export default function Index() {
             >
             <Box sx={style}>
                 <h1 style={{borderBottom: '3px solid red', paddingBottom: '20px', textAlign: 'center', fontSize: 30, color: 'white'}}>AJUDA VOLUNTÁRIA</h1>
-                <p style={{color: 'white'}}>
+                <Typography style={{color: 'white'}}>
                 Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
                 Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
                 Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
@@ -54,9 +54,11 @@ export default function Index() {
                 Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
                 Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
                 Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </p>
+                </Typography>
             </Box>
             </Modal>
+            <Section1>
+            </Section1>
         </div>
     )
 }
