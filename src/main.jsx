1b1872e8874345825f6f5cf719eb0be616dde7f5
Isaac Ramos <isaacdeferro@gmail.com>
2024-05-20 09:07:50 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './components/Header';
+import Header from './components/header';
 import Index from './index';
 import ErrorPage from './ErrorPage';
 import {
