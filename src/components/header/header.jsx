786ba0b972f580df9ccf1e66b103161467f6d908
Eduardo <04240581036@senacrs.edu.br>
2024-05-22 09:59:18 -0300

    feat: header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,16 +3,18 @@ import './style.css'
 export default function Header() {
     return(
       <header className="header">
-        <span className="ajuda-bg">Ajuda Voluntária</span>
-        <h1 className="ajuda-title"><span className="ajuda-span">Ajuda</span> Voluntária</h1>
-        <nav className='nav-bar'>
-            <ul>
-                <li><a id="inicio" href="default.asp">Início</a></li>
-                <li><a id="noticias" href="news.asp">Notícias</a></li>
-                <li><a id="Contato" href="contato.asp">Contato</a></li>
-                <li><a id="Sobre" href="sobre.asp">Sobre</a></li>
-            </ul>
-          </nav>
+        
+          <span className="esperanca-bg gradient">Esperança rs</span>
+          <h1 className="esperanca-title">esperança<span className="esperanca-span">rs</span></h1>
+
+          <nav>
+              <ul className='nav-bar'>
+                  <li><a id="inicio" href="default.asp">Início</a></li>
+                  <li><a id="noticias" href="news.asp">Notícias</a></li>
+                  <li><a id="Contato" href="contato.asp">Contato</a></li>
+                  <li><a id="Sobre" href="sobre.asp">Sobre</a></li>
+              </ul>
+            </nav>
      </header>
     )
 }
